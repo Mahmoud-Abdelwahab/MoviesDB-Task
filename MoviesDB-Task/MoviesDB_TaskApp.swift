9ea206117fb265a0bd7c45713297cc1d05b93293
Mahmoud-abdelwahab-iOS <152803379+Mahmoud-abdelwahab-iOS@users.noreply.github.com>
2024-04-11 13:06:16 +0200

    Initial Commit

---
+++ MoviesDB-Task/MoviesDB_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDB_TaskApp.swift
+//  MoviesDB-Task
+//
+//  Created by Mahmoud Abdelwahab on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDB_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
