aeb0234b7ce522777655d4abc8aa2eec6aa324c2
Mahmoud-abdelwahab-iOS <152803379+Mahmoud-abdelwahab-iOS@users.noreply.github.com>
2024-04-11 15:07:04 +0200

    init

---
+++ MoviesDB-Task/Models/Presentation/MovieListView/MovieList.swift
@@ -0,0 +1,24 @@
+//
+//  MovieList.swift
+//  MoviesDB-Task
+//
+//  Created by Mahmoud Abdelwahab on 11/04/2024.
+//
+
+import SwiftUI
+
+struct MovieList: View {
+    
+    var body: some View {
+        NavigationView {
+            List(0..<10) { item in
+                Text("movie \(item)")
+            }
+            .navigationBarTitle("Trending Movies")
+        }
+    }
+}
+
+#Preview {
+    MovieList()
+}
