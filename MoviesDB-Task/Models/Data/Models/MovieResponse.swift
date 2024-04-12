4273ff0256d4eb55af08452281d9f171995df453
Mahmoud-abdelwahab-iOS <152803379+Mahmoud-abdelwahab-iOS@users.noreply.github.com>
2024-04-12 17:24:05 +0200

    adding response

---
+++ MoviesDB-Task/Models/Data/Models/MovieResponse.swift
@@ -0,0 +1,23 @@
+//
+//  MovieResponse.swift
+//  MoviesDB-Task
+//
+//  Created by Mahmoud Abdelwahab on 12/04/2024.
+//
+
+import Foundation
+
+struct MovieListResponse: Decodable {
+    let page: Int
+    let results: [MovieResponse]
+    let totalPages: Int
+    let totalResults: Int
+}
+
+struct MovieResponse: Decodable {
+    let id: Int
+    let title: String
+    let overview: String
+    let releaseDate: String
+    let posterPath: String?
+}
